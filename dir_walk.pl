60b6e4a40806d040d41caa6aaac77eea0a491868
Shichen Wang <wangsc@tamu.edu>
2019-08-07 16:35:40 -0500

    backup

--- dir_walk.pl
+++ dir_walk.pl
@@ -5,18 +5,23 @@ die "perl $0 <dir>\n" unless @ARGV;
 
 my $p = &dir_walk($ARGV[0]);
 while (my $f = $p->()){
-    print $f, "\n"
+    print ($f . "\n");
 }
 
 sub dir_walk {
     my @fs = ();
     my $d = shift;
-    push @fs, $d;
+    push @fs, "$d";
     my $func = sub {
       my $p = shift @fs;
       return unless $p;
       if (-d $p){
-          my @arr= <$p/*>;
+          my @arr;
+          opendir(my $dh, $p) || die "Can't open $p: $!";
+          while (readdir $dh) {
+            push @arr, "$p/$_" unless $_ eq "." or $_ eq "..";
+          }
+          closedir $dh;
           push @fs, @arr;
       }
       return $p
